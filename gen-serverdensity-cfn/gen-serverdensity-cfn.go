e679d78b79d49b127ff0305d063934234eaac9df
Richard Guest <r.guest@gns.cri.nz>
2016-05-03 16:32:32 +1200

    Add optional list of SD site codes to include in security groups.

--- gen-serverdensity-cfn/gen-serverdensity-cfn.go
+++ gen-serverdensity-cfn/gen-serverdensity-cfn.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/quiffman/gen-secgroup-cfn"
 	"golang.org/x/net/html"
@@ -34,14 +35,15 @@ func check(err error) {
 }
 
 func main() {
-	var name, vpcId, protocol, port string
+	var name, vpcId, protocol, port, codes string
 	flag.StringVar(&name, "name", "ServerDensity", "Name to use for this auto-generated security group.")
 	flag.StringVar(&vpcId, "vpc", "", "The VPC ID to place this auto-generated security group in.")
 	flag.StringVar(&protocol, "protocol", "tcp", "The IP protocol name (tcp, udp, icmp) or number that these rules should apply to.")
 	flag.StringVar(&port, "port", "80", "The port number or port range to allow.")
+	flag.StringVar(&codes, "codes", "", "The comma separated list of ServerDensity location codes to add rules for, or all if left empty.")
 	flag.Parse()
 
-	ips, err := scrapePage("https://support.serverdensity.com/hc/en-us/articles/201091476-Monitoring-node-locations-and-IP-addresses")
+	ips, err := scrapePage("https://support.serverdensity.com/hc/en-us/articles/201091476-Monitoring-node-locations-and-IP-addresses", codes)
 	check(err)
 
 	t, err := cfn.GenTemplate(ips, name, vpcId, protocol, port)
@@ -53,7 +55,7 @@ func main() {
 	os.Stdout.Write(b)
 }
 
-func scrapePage(url string) ([]string, error) {
+func scrapePage(url string, codes string) ([]string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -67,6 +69,7 @@ func scrapePage(url string) ([]string, error) {
 	z := html.NewTokenizer(resp.Body)
 	defer resp.Body.Close()
 
+	var code string
 	for {
 		tt := z.Next()
 
@@ -77,8 +80,13 @@ func scrapePage(url string) ([]string, error) {
 		case tt == html.StartTagToken:
 			t := z.Token()
 
-			isLi := t.Data == "li"
-			if isLi {
+			if t.Data == "code" {
+				z.Next()
+
+				code = z.Token().Data
+			}
+
+			if t.Data == "li" {
 				//fmt.Println(t)
 				for _, a := range t.Attr {
 					//fmt.Println(a)
@@ -86,8 +94,10 @@ func scrapePage(url string) ([]string, error) {
 						z.Next()
 
 						i := z.Token().Data
-						if net.ParseIP(i) != nil {
-							ips = append(ips, fmt.Sprintf("%s/32", i))
+						if code != "" && strings.Contains(codes, code) {
+							if net.ParseIP(i) != nil {
+								ips = append(ips, fmt.Sprintf("%s/32", i))
+							}
 						}
 					}
 				}
@@ -122,4 +132,4 @@ func (aerr ApiError) Error() string {
 	return fmt.Sprintf("Get %s returned status %d, %s", aerr.URL, aerr.StatusCode, aerr.Body)
 }
 
-//  vim: set ts=4 sw=4 tw=0 et:
+//  vim: set ts=4 sw=4 tw=0 :
